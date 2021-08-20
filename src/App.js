1f58f386153aa2a4317fc80562aa8a0d32e7c751
1risS <iris.r.saladino@gmail.com>
2021-08-20 17:00:55 -0300

    coso

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ import sparkles1 from "./assets/star.gif"
 import star2 from "./assets/star2.gif"
 import heart from "./assets/heart2.gif"
 import sparkle from "./assets/sparkle04.gif"
+import stop0 from "./assets/stop0.jpg"
+import stop1 from "./assets/stop1.jpg"
 
 
 const videosRoot = `${process.env.PUBLIC_URL}/videos`;
@@ -22,6 +24,7 @@ const backVideos = [2, 4, 6, 8, 10, 12].map((i) => `${videosRoot}/${i}.webm`);
 function App() {
   const [video, setVideo] = useState(0);
   const [showButterflies, setShowButterflies] = useState(false);
+  const [showModal, setShowModal] = useState(false);
   const[showHeart, setShowHeart] = useState(false);
   const [showSparkle, setShowSparkle] = useState(false);
   const frontVideoRefs = frontVideos.map(() => createRef());
@@ -97,6 +100,16 @@ function App() {
     }, 15000);
   }
 
+  useEffect(() => {
+    console.log("showModal", showModal)
+  }, [showModal]);
+
+  const handleShowModal = () => {
+    setShowModal(true);
+  }
+
+
+
   const frontPlaying = video % 2 === 0;
   const backPlaying = video % 2 === 1;
 
@@ -117,9 +130,26 @@ function App() {
             <img className="wings" src={wings} alt="wings"></img>
           </div>
           <div className="title">Neko!</div>
-          <div className="space">WTF is this?</div>
+          <div className="space" onClick={handleShowModal}>WTF is this?</div>
         </div>
         <div className="contentCont">
+          {showModal && <div className="modal">
+            <div className="txt">
+              Este sitio es una exploración de la idea de "animación interactiva". El punto de partida fue la creación de un video en stop motion, con chroma, aprovechando que los browsers modernos <a href="https://developers.google.com/web/updates/2013/07/Alpha-transparency-in-Chrome-video" target="blank">soportan videos con transparencias</a> .
+            </div>
+            <div className="imgCont">
+              <img src={stop0} alt="chroma0" height="300px"></img>
+              <img src={stop1} alt="chroma1" height="300px"></img>
+            </div>
+            <div className="txt">
+              Para acelerar el proceso, la remoción del fondo fue hecha con <a href="https://medium.com/fnplus/blue-or-green-screen-effect-with-open-cv-chroma-keying-94d4a6ab2743" target="blank"> una librería de Python que usa OpenCV</a> pero el resultado no fue el esperado debido a que la técnica de iluminación debió ser mejor para que el algotimo pudiera segmentar la figura sin imperfecciones. 
+              <br></br>
+              El plano frontal general muestra el movimiento de la figura de un gato, dando cuenta de la profundidad del set. 
+              <br></br>
+              Una vez logrado el video, se cargó en la web. Era claro que podíamos profundizar en la idea de adelante-atrás de este gato. Para ello, cortamos el video en varios, de acuerdo a la posición y recorrido del personaje, y desarrollamos un lector secuencial que nos permite incorporar otros elementos visuales entre el "detrás" y el "frente" de la escena, logrando un fundido mayor del elemento visual en video con los componentes propios de la web e imágenes en diferentes formatos.
+            </div>
+            </div>
+          }
           {showButterflies && <img className="but butA" id="but" src={butterflies} alt="butterflies" />}
           
           {/* video 1 */}
